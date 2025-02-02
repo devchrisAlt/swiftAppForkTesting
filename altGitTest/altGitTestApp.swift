d352ad349a952248469224b7bdba8c2ba1ebe6ab
Christian Molina <devchris1314@gmail.com>
2025-02-01 19:40:58 -0500

    Initial Commit

---
+++ altGitTest/altGitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  altGitTestApp.swift
+//  altGitTest
+//
+//  Created by Cristian Molina on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct altGitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
